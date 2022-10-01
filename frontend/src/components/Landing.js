e9fd06616a8bf71eabcb26a07c79d97988e74b8e
Rimberse <kerimpltm@gmail.com>
2022-10-01 23:44:21 +0200

    Add Tailwind CSS dependency into the project, make tests

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './styles/landing.css';
+import '../styles/landing.css';
 
 const Landing = () => {
     return(
+        
         <div>
-
+            <h1>Enhance Your Workout Routine for Better Results</h1>
         </div>
     )
 }
