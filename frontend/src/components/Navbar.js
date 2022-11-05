73f09889b69cb187a0cbb94f475cd17fe5e03542
Rimberse <kerimpltm@gmail.com>
2022-11-05 22:30:55 +0100

    Add react-responsive dependency

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,14 @@ import React from "react";
 import '../styles/navbar.css';
 import Button from "./Button";
 import { UserCircleIcon } from '@heroicons/react/24/solid';
+import { useMediaQuery } from 'react-responsive';
 
 const Navbar = () => {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' })
     
     return (
         <div id="navbar" className="mb-8 border-2 border-b-indigo-700">
-            <div className="flex flex-row w-3/4 h-32 mx-auto">
+            {!isTabletOrMobile && <div className="flex flex-row w-3/4 h-32 mx-auto">
                 <div className="basis-1/2 justify-around items-center shrink-0"></div>
                 <div className="basis-1/2 justify-around items-center shrink-0">
                     <div className="flex flex-row-reverse w-full h-full justify-evenly items-center">
@@ -21,7 +23,7 @@ const Navbar = () => {
                         <Button Name={"Home"} />
                     </div>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
