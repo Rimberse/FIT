14ceace157d19a7327bb4a1fbf99ae4962b8c6d2
Rimberse <kerimpltm@gmail.com>
2022-11-05 18:59:02 +0100

    Implement the general layout of the navigation bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,8 +6,15 @@ const Navbar = () => {
     
     return (
         <div id="navbar">
-            <div className="navbar__user">
-                <UserCircleIcon className="h-10 w-10 text-indigo-700"/>
+            <div className="flex flex-row w-3/4 h-32 mx-auto my-8">
+                <div className="basis-1/2"></div>
+                <div className="basis-1/2">
+                    <div className="flex flex-row">
+                        <div className="navbar__user">
+                            <UserCircleIcon className="h-10 w-10 text-indigo-700"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
