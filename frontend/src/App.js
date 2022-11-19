a89c708ac6bb9a6194fe56d727d6638e9396fe05
Rimberse <kerimpltm@gmail.com>
2022-11-19 14:24:55 +0100

    Create Login and Register Page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute';
+import { AuthenticationProvider } from './services/AuthenticationContext';
 import Landing from './components/Landing';
+
 
 function App() {
   return (
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import AuthenticationContext from "../services/AuthenticationContext";
+
+const Login = () => {
+    const { loginUser } = useContext(AuthenticationContext);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+        username.length > 0 && loginUser(username, password);
+    }
+
+    return(
+        <section>
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <hr />
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" placeholder="Enter Username"></input>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder="Enter Password"></input>
+                <button type="submit">Login</button>
+            </form>
+        </section>
+    );
+}
+
+export default Login;
