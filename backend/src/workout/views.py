fb6bd48a9db560d1234105cb5e53e9b4acf1123d
Rimberse <kerimpltm@gmail.com>
2022-12-05 02:40:53 +0100

    Create View class for Exercise with GET & POST endpoints

--- backend/src/workout/views.py
+++ backend/src/workout/views.py
@@ -16,7 +16,7 @@ class WorkoutApiView(APIView):
         # Return all workout for given requested user
         workouts = Workout.objects.filter(user = request.user.id)
         serializer = WorkoutSerializer(workouts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status = status.HTTP_200_OK)
 
     # Create workout
     def post(self, request, *args, **kwargs):
@@ -31,6 +31,32 @@ class WorkoutApiView(APIView):
         serializer = WorkoutSerializer(data = data)
         if (serializer.is_valid()):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class ExerciseApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Retrieve all exercises
+    def get(self, request, *args, **kwargs):
+        # Return all workout for given requested user
+        exercises = Exercise.objects.all()
+        serializer = ExerciseSerializer(exercises, many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    # Create exercise
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.data.get('name'), 
+            'instructions': request.data.get('instructions'),
+            'workout': request.data.get('workout')
+        }
+
+        serializer = ExerciseSerializer(data = data)
+        if (serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
