21a15572dec78cbbb8119eb72ac3e5360590bae7
Rimberse <kerimpltm@gmail.com>
2023-01-08 01:35:24 +0100

    Alter Exercise model to allow users adding same exercises to their workout sessions, define URL for sets

--- backend/src/workout/models.py
+++ backend/src/workout/models.py
@@ -19,7 +19,7 @@ class Workout(models.Model):
 
 # One workout may contain multiple exercises composed either from a dedicated program, or from manually created by user
 class Exercise(models.Model):
-    name = models.CharField(max_length = 50, unique = True)
+    name = models.CharField(max_length = 50)
     instructions = models.TextField(max_length = 2048)
     workout = models.ForeignKey(Workout, on_delete = models.CASCADE)          # Many to one
 
--- backend/src/workout/urls.py
+++ backend/src/workout/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 
 from .views import (
     WorkoutApiView,
-    ExerciseApiView
+    ExerciseApiView,
+    SetApiView
 )
 
 urlpatterns = [
     path('workout/', WorkoutApiView.as_view()),
     path('exercise/', ExerciseApiView.as_view()),
+    path('set/', SetApiView.as_view())
 ]
