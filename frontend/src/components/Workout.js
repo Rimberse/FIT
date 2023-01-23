c1634c9d43ca865c83a7aab1522c9fe95add17a4
Rimberse <kerimpltm@gmail.com>
2023-01-23 21:37:26 +0100

    Initial commit

---
+++ frontend/src/components/Workout.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import useAxios from "../utils/UseAxios";
+
+const Workout = () => {
+
+    return(
+        <div>
+            <h1>Workout</h1>
+        </div>
+    );
+};
+
+export default Workout;
