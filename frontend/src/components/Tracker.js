a4fb1ee29e18c433fdda91a9dd2072a4b5e4b815
Rimberse <kerimpltm@gmail.com>
2023-01-24 07:45:19 +0100

    Implement Interval class, which will be used to count Ticks of a Counter (tracker's HH:MM:SS counter)

--- frontend/src/components/Tracker.js
+++ frontend/src/components/Tracker.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAxios from "../utils/UseAxios";
+import SelfAdjustingInterval from "../utils/SelfAdjustingInterval";
 
 const Tracker = () => {
     const [response, setResponse] = useState('');
     const [isTrackerVisible, setIsTrackerVisible] = useState(false);
+    const [time, setTimer] = useState('');
     const api = useAxios();
 
-    return(
-        <h1>Test</h1>
+    return (
+        <div>
+            <h1>Test</h1>
+            <div>{time}</div>
+        </div>
     );
 }
 
