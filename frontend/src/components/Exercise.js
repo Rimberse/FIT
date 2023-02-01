e7654a2c3dc05d584aa1d21c95a5d2b868799dd0
Rimberse <kerimpltm@gmail.com>
2023-02-01 23:00:58 +0100

    Improve styling of tracker's grid, add more elements & styles

--- frontend/src/components/Exercise.js
+++ frontend/src/components/Exercise.js
@@ -3,10 +3,10 @@ import React from "react";
 const Exercise = () => {
 
     return (
-        <div className="table-fixed grid grid-rows-4 gap-4 divide-y divide-slate-500 rounded shadow-lg shadow-black w-3/4 max-h-fit mx-auto my-20">
-            <input type="text" id="exercise" name="exercise" placeholder="Enter exercise name" className="p-2" />
+        <div className="table-fixed grid grid-rows-4 divide-y divide-stone-700 rounded shadow-md shadow-black text-center align-middle text-white text-base font-medium tracking-wide uppercase w-3/4 max-h-fit bg-clip-padding bg-stone-900 mx-auto my-20">
+            <input type="text" id="exercise" name="exercise" placeholder="Enter exercise name" className="p-2 text-center bg-stone-700 justify-self-start w-1/4" />
             <div>
-                <div className="grid grid-rows-1 grid-cols-5 gap-4 text-center align-middle">
+                <div className="grid grid-rows-1 grid-cols-5 gap-4">
                     <span className="p-2">Set</span>
                     <span className="p-2">Previous</span>
                     <span className="p-2">Kg / Lbs</span>
@@ -15,15 +15,15 @@ const Exercise = () => {
                 </div>
             </div>
             <div>
-                <div className="grid grid-rows-1 grid-cols-5 gap-4 text-center align-middle">
+                <div className="grid grid-flow-row auto-rows-max grid-cols-5 gap-4">
                     <span className="p-2">1</span>
                     <span className="p-2">...</span>
-                    <input type="number" id="weight" name="weight" required min={1} className="p-2" />
-                    <input type="number" id="repetitions" name="repetitions" required min={1} className="p-2" />
-                    <input type="checkbox" id="status" name="status" required  className="p-2"/>
+                    <input type="number" id="weight" name="weight" required min={1} className="p-2 text-center bg-stone-700" />
+                    <input type="number" id="repetitions" name="repetitions" required min={1} className="p-2 text-center bg-stone-700" />
+                    <input type="checkbox" id="status" name="status" required  className="p-2 text-center bg-stone-700"/>
                 </div>
             </div>
-            <button>Add set</button>
+            <button className="p-2 text-center bg-white text-stone-900 justify-self-end w-1/4">Add set</button>
         </div>
     );
 }
