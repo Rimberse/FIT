8820f99e9bee702d7cbd5b53a2938f3abe64b0ad
Rimberse <kerimpltm@gmail.com>
2023-02-01 16:55:46 +0100

    Lay out the grid elements, add styling to it's elements

--- frontend/src/components/Exercise.js
+++ frontend/src/components/Exercise.js
@@ -3,25 +3,24 @@ import React from "react";
 const Exercise = () => {
 
     return (
-        <div>
-            <h2>Exercise name</h2>
+        <div className="table-fixed grid grid-rows-4 gap-4 divide-y divide-slate-500 rounded shadow-lg shadow-black w-3/4 max-h-fit mx-auto my-20">
+            <input type="text" id="exercise" name="exercise" placeholder="Enter exercise name" className="p-2" />
             <div>
-                <div>
-                    <span>Set</span>
-                    <span>Previous</span>
-                    <span>Kg / Lbs</span>
-                    <span>Reps</span>
-                    <span>Status</span>
+                <div className="grid grid-rows-1 grid-cols-5 gap-4 text-center align-middle">
+                    <span className="p-2">Set</span>
+                    <span className="p-2">Previous</span>
+                    <span className="p-2">Kg / Lbs</span>
+                    <span className="p-2">Reps</span>
+                    <span className="p-2">Status</span>
                 </div>
             </div>
             <div>
-                <div>
-                    <span></span>
-                    <span>...</span>
-                    <label htmlFor="name">Weight</label>
-                    <input type="number" id="weight" name="weight" required min={1} />
-                    <input type="number" id="repetitions" name="repetitions" required min={1} />
-                    <input type="checkbox" id="status" name="status" required />
+                <div className="grid grid-rows-1 grid-cols-5 gap-4 text-center align-middle">
+                    <span className="p-2">1</span>
+                    <span className="p-2">...</span>
+                    <input type="number" id="weight" name="weight" required min={1} className="p-2" />
+                    <input type="number" id="repetitions" name="repetitions" required min={1} className="p-2" />
+                    <input type="checkbox" id="status" name="status" required  className="p-2"/>
                 </div>
             </div>
             <button>Add set</button>
