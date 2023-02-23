38f3134b6eeda5f3537b6e9c0221872967e128bc
Rimberse <kerimpltm@gmail.com>
2023-02-23 21:10:38 +0100

    Slightly change layout, improve spacing out of elements

--- frontend/src/components/Exercise.js
+++ frontend/src/components/Exercise.js
@@ -32,12 +32,12 @@ const Exercise = () => {
         <div className="grid auto-rows-auto divide-y divide-stone-700">
             <input type="text" id="exercise" name="exercise" placeholder="Enter exercise name" className="p-2 text-center bg-stone-700 justify-self-start m-4 w-1/4 border shadow-sm border-stone-700 placeholder-white focus:outline-none focus:border-stone-300 focus:ring-stone-300 rounded-lg focus:ring-1" />
             <div className="grid grid-flow-col auto-rows-max grid-cols-[1fr,1fr,1fr,1fr,1fr,max-content] gap-x-8 place-content-evenly">
-                <span className="p-2 inline-flex justify-center items-center">Set</span>
-                <span className="p-2 inline-flex justify-center items-center">Previous</span>
-                <span className="p-2 inline-flex justify-center items-center">Kg / Lbs</span>
-                <span className="p-2 inline-flex justify-center items-center">Reps</span>
-                <span className="p-2 inline-flex justify-center items-center">Status</span>
-                <span className="h-7 w-7 justify-self-end self-center mr-5"></span>
+                <span className="p-2 inline-flex justify-center items-center my-2">Set</span>
+                <span className="p-2 inline-flex justify-center items-center my-2">Previous</span>
+                <span className="p-2 inline-flex justify-center items-center my-2">Kg / Lbs</span>
+                <span className="p-2 inline-flex justify-center items-center my-2">Reps</span>
+                <span className="p-2 inline-flex justify-center items-center my-2">Status</span>
+                <span className="h-7 w-7 justify-self-end self-center mr-5 my-2"></span>
             </div>
             {sets.map((set, index) => <Set ref={ref => refs.current[index] = ref} key={index} setNumber={index + 1} sets={sets} setSets={setSets} removeSet={onRemoveSet} />)}
             <button onClick={onAddSet} className="p-4 text-center bg-stone-300 text-stone-900 justify-self-end m-4 w-1/4 font-bold tracking-wide uppercase hover:bg-stone-700 hover:text-white rounded-md">Add set</button>
--- frontend/src/components/Set.js
+++ frontend/src/components/Set.js
@@ -34,11 +34,11 @@ const Set = forwardRef(({ setNumber, sets, setSets, removeSet }, ref) => {
 
     return (
         <div className="grid grid-flow-col auto-rows-max grid-cols-[1fr,1fr,1fr,1fr,1fr,max-content] gap-x-8 place-content-evenly">
-            <span className="p-2 inline-flex justify-self-center self-center my-4">{setNumber}</span>
-            <span className="p-2 inline-flex justify-self-center self-center my-4">{sets[setNumber - 1]['previous']}</span>
-            <input type="number" id="weight" name="weight" ref={weight} required min={1} className="p-2 inline-flex justify-self-center self-center text-center bg-stone-700 border shadow-sm border-stone-700 placeholder-white focus:outline-none focus:border-stone-300 focus:ring-stone-300 rounded-lg focus:ring-1 my-4" defaultValue={sets[setNumber - 1]['kilograms'] || ''} key={"kg: " + sets[setNumber - 1]['kilograms']} />
-            <input type="number" id="repetitions" name="repetitions" ref={repetitions} required min={0} step={1} className="p-2 inline-flex justify-self-center self-center text-center bg-stone-700 border shadow-sm border-stone-700 placeholder-white focus:outline-none focus:border-stone-300 focus:ring-stone-300 rounded-lg focus:ring-1 my-4" defaultValue={sets[setNumber - 1]['repetitions'] || ''} key={"reps: " + sets[setNumber - 1]['repetitions']} />
-            <input type="checkbox" id="status" name="status" ref={completion} required className="p-2 h-7 w-7 inline-flex justify-self-center self-center text-center bg-stone-700 my-4" defaultChecked={sets[setNumber - 1]['isFinished'] || false} key={sets[setNumber - 1]['isFinished']}/>
+            <span className="p-2 inline-flex justify-self-center self-center my-2">{setNumber}</span>
+            <span className="p-2 inline-flex justify-self-center self-center my-2">{sets[setNumber - 1]['previous']}</span>
+            <input type="number" id="weight" name="weight" ref={weight} required min={1} className="p-2 inline-flex justify-self-center self-center text-center bg-stone-700 border shadow-sm border-stone-700 placeholder-white focus:outline-none focus:border-stone-300 focus:ring-stone-300 rounded-lg focus:ring-1 my-2" defaultValue={sets[setNumber - 1]['kilograms'] || ''} key={"kg: " + sets[setNumber - 1]['kilograms']} />
+            <input type="number" id="repetitions" name="repetitions" ref={repetitions} required min={0} step={1} className="p-2 inline-flex justify-self-center self-center text-center bg-stone-700 border shadow-sm border-stone-700 placeholder-white focus:outline-none focus:border-stone-300 focus:ring-stone-300 rounded-lg focus:ring-1 my-2" defaultValue={sets[setNumber - 1]['repetitions'] || ''} key={"reps: " + sets[setNumber - 1]['repetitions']} />
+            <input type="checkbox" id="status" name="status" ref={completion} required className="p-2 h-7 w-7 inline-flex justify-self-center self-center text-center bg-stone-700 my-2" defaultChecked={sets[setNumber - 1]['isFinished'] || false} key={sets[setNumber - 1]['isFinished']}/>
             <button onClick={() => onRemoveSet()} className="h-7 w-7 justify-self-end self-center text-stone-300 mr-5 hover:cursor-pointer"><TrashIcon /></button>
         </div>
     );
