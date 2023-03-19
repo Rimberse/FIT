15bc2008857264f3a67da17d625aabce7fd2e41c
Rimberse <kerimpltm@gmail.com>
2023-03-19 03:49:32 +0100

    Add more icons for visual cues, improve styling

--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -1,5 +1,5 @@
 import React, { createRef, useRef, useState } from "react";
-import Navbar from "./Navbar";
+import { CheckCircleIcon, NoSymbolIcon, ClockIcon, CalendarIcon } from '@heroicons/react/24/outline';
 import { ChevronDoubleDownIcon, ChevronDoubleUpIcon } from '@heroicons/react/24/solid';
 
 const Entry = ({ workout }) => {
@@ -23,20 +23,22 @@ const Entry = ({ workout }) => {
     };
 
     return (
-        <div className="my-5">
+        <div className="rounded shadow-sm shadow-violet-500 bg-clip-padding p-8 mx-auto my-6">
             <div className="flex content-center text-3xl text-center align-middle">
-                <span className="justify-self-start mr-auto p-3">{workout.name}</span>
-                <span className="justify-self-end p-3">{workout.length}</span>
-                <span className="justify-self-end p-3">{workout.date}</span>
+                <span className="justify-self-start self-center mr-auto p-3">{workout.name}</span>
+                <ClockIcon className="justify-self-end self-center inline-block w-8 h-8" />
+                <span className="justify-self-end self-center p-3 mr-3">{workout.length}</span>
+                <CalendarIcon className="justify-self-end self-center inline-block w-8 h-8" />
+                <span className="justify-self-end self-center p-3">{workout.date}</span>
             </div>
             <button onClick={() => onToggleExercisesVisibility()} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500 my-2">{exercisesRevealed ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
-            <div className="text-xl">
+            <div className="text-violet-500 text-xl font-normal">
                 <ul ref={exercises} className="hidden">
                     {workout.exercises.map((exercise, index) => <li key={index}>
-                            <div className="px-9 py-3">{exercise.name}</div>
-                            <p className="text-justify px-9 py-3">{exercise.instructions}</p>
+                            <div className="font-medium px-9 py-3">{exercise.name}</div>
+                            <blockquote><p className="text-justify indent-8 italic px-9 py-3">{exercise.instructions}</p></blockquote>
                             <button onClick={() => onToggleSetsVisibility(index)} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500 ml-9 my-2">{setsRevealed[index] ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
-                            {setsRevealed[index] && <div className="flex justify-between content-center text-lg text-center px-16 py-3">
+                            {setsRevealed[index] && <div className="flex justify-between content-center text-lg font-medium text-center px-16 py-3">
                                 <span className="w-1/4">Set</span>
                                 <span className="w-1/4">Kg</span>
                                 <span className="w-1/4">Reps</span>
@@ -48,7 +50,7 @@ const Entry = ({ workout }) => {
                                             <span className="w-1/4">{index + 1}</span>
                                             <span className="w-1/4">{set.kilograms}</span>
                                             <span className="w-1/4">{set.repetitions}</span>
-                                            <span className="w-1/4">{set.isFinished ? 'Yes' : 'No'}</span>
+                                            <span className="w-1/4">{set.isFinished ? <CheckCircleIcon className="w-8 h-8 mx-auto" /> : <NoSymbolIcon className="w-8 h-8 mx-auto" />}</span>
                                         </div>
                                     </li>
                                 )}
--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -1,26 +1,9 @@
-import React, { createRef, useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
-import { ChevronDoubleDownIcon, ChevronDoubleUpIcon } from '@heroicons/react/24/solid';
 import Entry from "./Entry";
 
 const History = () => {
     const [workouts, setWorkouts] = useState([]);
-    const [exercisesRevealed, setExercisesRevealed] = useState(false);
-    const [setsRevealed, setSetsRevealed] = useState([]);
-    const exercises = createRef();
-    const sets = useRef([]);
-
-    const onToggleExercisesVisibility = () => {
-        exercises.current.style.display = (exercises.current.style.display == 'block') ? 'none' : 'block';
-        setExercisesRevealed(!exercisesRevealed);
-    };
-
-    const onToggleSetsVisibility = exerciseNumber => {
-        sets.current[exerciseNumber].style.display = (sets.current[exerciseNumber].style.display == 'block') ? 'none' : 'block';
-        const newSetsRevealed = setsRevealed.slice(0, exerciseNumber);
-        newSetsRevealed.push(!setsRevealed[exerciseNumber]);
-        setSetsRevealed(newSetsRevealed);
-    };
 
     useEffect(() => {
         setWorkouts([
@@ -160,7 +143,7 @@ const History = () => {
     return (
         <div>
             <Navbar />
-            <div className="rounded shadow-sm shadow-violet-500 text-violet-700 text-base font-medium tracking-wide max-h-fit bg-clip-padding p-8 w-3/4 mx-auto my-48">
+            <div className="text-violet-700 text-base font-medium tracking-wide max-h-fit bg-clip-padding p-8 w-3/4 mx-auto my-40">
                 {workouts.map((workout, index) => <Entry workout={workout} key={index} />)}
             </div>
         </div>
