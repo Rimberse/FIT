1f42881d21ce62ddc675f0e24efb3accb1df0f46
Rimberse <kerimpltm@gmail.com>
2023-03-19 02:45:39 +0100

    Improve visual appeareance & styling of history entries

--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -14,77 +14,47 @@ const Entry = ({ workout }) => {
     };
 
     const onToggleSetsVisibility = exerciseNumber => {
+        // Toggles display value of html element
         sets.current[exerciseNumber].style.display = (sets.current[exerciseNumber].style.display == 'block') ? 'none' : 'block';
-        const newSetsRevealed = setsRevealed.slice(0, exerciseNumber);
-        newSetsRevealed.push(!setsRevealed[exerciseNumber]);
+        // Makes a copy of existing array of references and toggles visibility of sets of given exercise
+        const newSetsRevealed = setsRevealed.slice();
+        newSetsRevealed[exerciseNumber] = !setsRevealed[exerciseNumber];
         setSetsRevealed(newSetsRevealed);
     };
 
     return (
         <div className="my-5">
             <div className="flex content-center text-3xl text-center align-middle">
-                <span className="justify-self-start mr-auto p-3">Workout name</span>
-                <span className="justify-self-end p-3">Length</span>
-                <span className="justify-self-end p-3">Date</span>
+                <span className="justify-self-start mr-auto p-3">{workout.name}</span>
+                <span className="justify-self-end p-3">{workout.length}</span>
+                <span className="justify-self-end p-3">{workout.date}</span>
             </div>
-            <button onClick={() => onToggleExercisesVisibility()} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500">{exercisesRevealed ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
+            <button onClick={() => onToggleExercisesVisibility()} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500 my-2">{exercisesRevealed ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
             <div className="text-xl">
                 <ul ref={exercises} className="hidden">
-                    <li>
-                        <div className="px-9 py-3">Exercise name</div>
-                        <p className="text-justify px-9 py-3">Exercise instructions</p>
-                        <div className="flex justify-between content-center text-lg text-center px-16 py-3">
-                            <span className="w-1/5">Set</span>
-                            <span className="w-1/5">Previous</span>
-                            <span className="w-1/5">Kg</span>
-                            <span className="w-1/5">Reps</span>
-                            <span className="w-1/5">Completion</span>
-                        </div>
-                        <button onClick={() => onToggleSetsVisibility(0)} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500">{setsRevealed[0] ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
-                        <ul ref={ref => sets.current[0] = ref} className="hidden">
-                            <li>
-                                <div className="flex justify-between content-center text-base text-center px-16 py-3">
-                                    <span className="w-1/5">1</span>
-                                    <span className="w-1/5">...</span>
-                                    <span className="w-1/5">60</span>
-                                    <span className="w-1/5">7</span>
-                                    <span className="w-1/5">Yes</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <div className="px-9 py-3">Exercise name</div>
-                        <p className="text-justify px-9 py-3">Exercise instructions</p>
-                        <div className="flex justify-between content-center text-lg text-center px-16 py-3">
-                            <span className="w-1/5">Set</span>
-                            <span className="w-1/5">Previous</span>
-                            <span className="w-1/5">Kg</span>
-                            <span className="w-1/5">Reps</span>
-                            <span className="w-1/5">Completion</span>
-                        </div>
-                        <button onClick={() => onToggleSetsVisibility(1)} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500">{setsRevealed[1] ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
-                        <ul ref={ref => sets.current[1] = ref} className="hidden">
-                            <li>
-                                <div className="flex justify-between content-center text-base text-center px-16 py-3">
-                                    <span className="w-1/5">1</span>
-                                    <span className="w-1/5">...</span>
-                                    <span className="w-1/5">60</span>
-                                    <span className="w-1/5">7</span>
-                                    <span className="w-1/5">Yes</span>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="flex justify-between content-center text-base text-center px-16 py-3">
-                                    <span className="w-1/5">2</span>
-                                    <span className="w-1/5">...</span>
-                                    <span className="w-1/5">60</span>
-                                    <span className="w-1/5">7</span>
-                                    <span className="w-1/5">No</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
+                    {workout.exercises.map((exercise, index) => <li key={index}>
+                            <div className="px-9 py-3">{exercise.name}</div>
+                            <p className="text-justify px-9 py-3">{exercise.instructions}</p>
+                            <button onClick={() => onToggleSetsVisibility(index)} className="h-14 w-14 hover:cursor-pointer p-3 rounded-full shadow-sm shadow-violet-500 ml-9 my-2">{setsRevealed[index] ? <ChevronDoubleUpIcon /> : <ChevronDoubleDownIcon />}</button>
+                            {setsRevealed[index] && <div className="flex justify-between content-center text-lg text-center px-16 py-3">
+                                <span className="w-1/4">Set</span>
+                                <span className="w-1/4">Kg</span>
+                                <span className="w-1/4">Reps</span>
+                                <span className="w-1/4">Completed</span>
+                            </div>}
+                            <ul ref={ref => sets.current[index] = ref} className="hidden">
+                                {exercise.sets.map((set, index) => <li key={index}>
+                                        <div className="flex justify-between content-center text-base text-center px-16 py-3">
+                                            <span className="w-1/4">{index + 1}</span>
+                                            <span className="w-1/4">{set.kilograms}</span>
+                                            <span className="w-1/4">{set.repetitions}</span>
+                                            <span className="w-1/4">{set.isFinished ? 'Yes' : 'No'}</span>
+                                        </div>
+                                    </li>
+                                )}
+                            </ul>
+                        </li>
+                    )}
                 </ul>
             </div>
         </div>
